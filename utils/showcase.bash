1c68281bccc625ef7ebf1ccb6d99cfa9076936a1
Michael Darakananda <pongad@google.com>
2018-10-04 12:53:53 -0700

    wait for showcase to exit to avoid zombies

--- utils/showcase.bash
+++ utils/showcase.bash
@@ -68,6 +68,8 @@ showcase_pid=$!
 
 stop_showcase() {
 	kill $showcase_pid
+	# Wait for the process to die, but don't report error from the kill.
+	wait $showcase_pid || true
 }
 trap stop_showcase EXIT
 
