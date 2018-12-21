bbd01ef2e9f3c4f6315cfb622eb650dc2dace5e9
Noah Dietz <ndietz@google.com>
2018-12-20 16:33:26 -0800

    gcli: mv context w/other stdlib

--- internal/gencli/root_file.go
+++ internal/gencli/root_file.go
@@ -26,6 +26,7 @@ const (
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -33,7 +34,6 @@ import (
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/spf13/cobra"
-	"context"
 )
 
 var Verbose, OutputJSON bool
