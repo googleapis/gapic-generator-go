af851400ce080cdd3cf5f23125e67081b807c6fe
Noah Dietz <ndietz@google.com>
2019-02-20 13:05:54 -0800

    add showcase integration
    * add gapic-showcase Echo service testing
    * update showcase.bash

--- utils/showcase.bash
+++ utils/showcase.bash
@@ -21,39 +21,26 @@ if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
 	return
 fi
 
-# Setup a test workspace. We gitignore this.
-rm -rf showcase-testdir
-mkdir showcase-testdir/
-
-cd showcase-testdir
-go mod init showcase-test
-go mod edit -require=github.com/googleapis/gapic-showcase@v0.0.7
+pushd showcase
+rm -rf gen
+mkdir gen
 
 protoc \
-	--go_gapic_out . \
-	--go_gapic_opt 'go-gapic-package=cloud.google.com/go/showcase/apiv1alpha2;showcase' \
-	--descriptor_set_in=<(curl -sSL https://github.com/googleapis/gapic-showcase/releases/download/v0.0.7/gapic-showcase-0.0.7.desc) \
-	google/showcase/v1alpha2/echo.proto
+  --go_out=plugins=grpc:./gen \
+	--go_gapic_out ./gen \
+	--go_gapic_opt 'go-gapic-package=cloud.google.com/go/showcase/apiv1alpha3;showcase' \
+	--descriptor_set_in=<(curl -sSL https://github.com/googleapis/gapic-showcase/releases/download/v0.0.11/gapic-showcase-0.0.11.desc) \
+	google/showcase/v1alpha3/echo.proto
 
-# TODO(pongad): Move this file into this repository once we deprecate the old generator.
-curl -sSL https://raw.githubusercontent.com/googleapis/gapic-generator/753ff9d8a04a59962f3b8c2c06cb79be7df344c8/showcase/go/showcase_integration_test.go \
-	> showcase_integration_test.go
+pushd gen/cloud.google.com/go/showcase
+go mod init cloud.google.com/go/showcase
+popd
+pushd gen/github.com/googleapis/gapic-showcase
+go mod init github.com/googleapis/gapic-showcase
+popd
 
-# If the package name of the generated package is "a/b/c", we write files under directory "$OUT/a/b/c",
-# where OUT is the directory passed to protoc. Prior to Go 1.11, we set OUT=$GOPATH/src.
-# This works well because that's where we expect to find the package a/b/c.
-#
-# This plays badly with modules however. In `go mod init` above, we set our module name to "showcase-test",
-# and set module root to showcase-testdir (because that's where the file is). If we set OUT=showcase-testdir,
-# we generate the files to showcase-testdir/a/b/c, but the import path of this package would actually be showcase-testdir/a/b/c,
-# not a/b/c as expected from the previous paragraph. We have to rewrite imports to keep this working.
-#
-# We copy this behavior from protoc-gen-go, so they're probably having the same problem with modules. Therefore...
-# TODO(pongad): figure out what protoc-gen-go is doing to solve this then do the same thing.
-find -name '*.go' | xargs sed -i 's,cloud.google.com/go/showcase/apiv1alpha2,showcase-test/&,g'
-
-curl -sSL https://github.com/googleapis/gapic-showcase/releases/download/v0.0.7/gapic-showcase-0.0.7-linux-amd64.tar.gz | tar xz
-./gapic-showcase start &
+curl -sSL https://github.com/googleapis/gapic-showcase/releases/download/v0.0.11/gapic-showcase-0.0.11-linux-amd64.tar.gz | tar xz
+./gapic-showcase run &
 showcase_pid=$!
 
 stop_showcase() {
@@ -64,3 +51,4 @@ stop_showcase() {
 trap stop_showcase EXIT
 
 go test -count=1 ./...
+popd
