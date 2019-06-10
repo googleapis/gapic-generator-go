2bf9f155a055d185c0ff026168a0faa27d3abd53
Noah Dietz <noahdietz@users.noreply.github.com>
2019-06-10 10:00:21 -0700

    chore: remove vendor'd googleapi pb.go files (#141)
    gapic: remove consumption of Package annotation b.c it won't be accepted

--- internal/gengapic/gengapic.go
+++ internal/gengapic/gengapic.go
@@ -89,29 +89,16 @@ func Gen(genReq *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, er
 	g.init(genReq.ProtoFile)
 
 	var genServs []*descriptor.ServiceDescriptorProto
-	var ePkg *annotations.Package
 	for _, f := range genReq.ProtoFile {
 		if !strContains(genReq.FileToGenerate, f.GetName()) {
 			continue
 		}
 		genServs = append(genServs, f.Service...)
-
-		// TODO(pongad): check if first-one-wins is the right strategy here.
-		if ePkg == nil {
-			if em, err := proto.GetExtension(f.GetOptions(), annotations.E_ClientPackage); err == nil {
-				ePkg = em.(*annotations.Package)
-			}
-		}
 	}
 
-	// favor annotations over service config
-	if ePkg != nil {
-		// Without this, the doc is going to be a little bad but this is not an error.
-		nameParts := append([]string(nil), ePkg.Namespace...)
-		nameParts = append(nameParts, ePkg.Title, "API")
-		g.apiName = strings.Join(nameParts, " ")
-	} else if g.serviceConfig != nil {
-		// TODO(ndietz) remove this once metadata/packaging annotations are accepted
+	if g.serviceConfig != nil {
+		// TODO(ndietz) remove this if some metadata/packaging
+		// annotations are ever accepted
 		g.apiName = g.serviceConfig.Title
 	}
 
