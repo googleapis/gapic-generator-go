c5a30fcc9e5e51e567fc68f0fdc48678d8b22659
Hanzhen Yi <hzyi@google.com>
2019-07-17 10:35:41 -0700

    remove churn

--- cmd/gen-go-sample/inittree.go
+++ cmd/gen-go-sample/inittree.go
@@ -51,11 +51,6 @@ type initType struct {
 	// NOTE(pongad): This func signature might seem too general. I think it is just general enough
 	// to deal with enums and bytes. Time will tell.
 	valFmt func(*generator, string) (string, error)
-
-	// If the type is a Go native type, native records the short name of the type.
-	// For now, nativeType is only used to check whether the commonly used variable `err` for error
-	// handling has been defined before.
-	native string
 }
 
 // initTree represents a node in the initialization tree.
