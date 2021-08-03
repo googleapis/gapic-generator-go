81919df0015d807e072af5707bb08834b52c7962
noahdietz <ndietz@google.com>
2021-08-03 10:50:49 -0700

    replace testing proto

--- showcase/showcase.bash
+++ showcase/showcase.bash
@@ -42,6 +42,7 @@ protoc \
 	google/showcase/v1beta1/echo.proto \
 	google/showcase/v1beta1/identity.proto \
 	google/showcase/v1beta1/sequence.proto \
+	google/showcase/v1beta1/testing.proto \
 	google/showcase/v1beta1/messaging.proto \
 	google/showcase/v1beta1/compliance.proto
 
