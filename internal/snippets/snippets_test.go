e9d8f8d55c2c788d34aeabf7222edefa517ef6cf
Chris Smith <chrisdsmith@google.com>
2023-02-21 13:12:17 -0700

    Update snippets_test to use go-cmp

--- internal/snippets/snippets_test.go
+++ internal/snippets/snippets_test.go
@@ -17,7 +17,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/googleapis/gapic-generator-go/internal/snippets/metadata"
+	"google.golang.org/protobuf/proto"
 )
 
 var protoPkg = "google.cloud.bigquery.migration.v2"
@@ -47,6 +49,7 @@ func TestNewMetadata(t *testing.T) {
 }
 
 func TestToMetadataJSON(t *testing.T) {
+	// Build fixture
 	sm, err := NewMetadata(protoPkg, libPkg, pkgName)
 	if err != nil {
 		t.Fatal(err)
@@ -63,127 +66,75 @@ func TestToMetadataJSON(t *testing.T) {
 		sm.AddParams(serviceName, methodName, "mypackage."+methodName+"Request")
 	}
 
+	// Build expectation
+	want := &metadata.Index{
+		ClientLibrary: &metadata.ClientLibrary{
+			Name:     libPkg,
+			Version:  VersionPlaceholder,
+			Language: metadata.Language_GO,
+			Apis: []*metadata.Api{
+				{
+					Id:      protoPkg,
+					Version: "v2",
+				},
+			},
+		},
+	}
+	for i := 0; i < 2; i++ {
+		snp := &metadata.Snippet{
+			RegionTag:   fmt.Sprintf("bigquerymigration_v2_generated_Foo%dService_Bar%dMethod_sync", i, i),
+			Title:       fmt.Sprintf("bigquerymigration Bar%dMethod Sample", i),
+			Description: fmt.Sprintf("Bar%dMethod doc\nNew line.", i),
+			File:        fmt.Sprintf("Foo%dClient/Bar%dMethod/main.go", i, i),
+			Language:    metadata.Language_GO,
+			Canonical:   false,
+			Origin:      *metadata.Snippet_API_DEFINITION.Enum(),
+			ClientMethod: &metadata.ClientMethod{
+				ShortName:  fmt.Sprintf("Bar%dMethod", i),
+				FullName:   fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dClient.Bar%dMethod", i, i),
+				Async:      false,
+				ResultType: fmt.Sprintf("mypackage.Bar%dMethodResult", i),
+				Client: &metadata.ServiceClient{
+					ShortName: fmt.Sprintf("Foo%dClient", i),
+					FullName:  fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dClient", i),
+				},
+				Method: &metadata.Method{
+					ShortName: fmt.Sprintf("Bar%dMethod", i),
+					FullName:  fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dService.Bar%dMethod", i, i),
+					Service: &metadata.Service{
+						ShortName: fmt.Sprintf("Foo%dService", i),
+						FullName:  fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dService", i),
+					},
+				},
+				Parameters: []*metadata.ClientMethod_Parameter{
+					{
+						Type: "context.Context",
+						Name: "ctx",
+					},
+					{
+						Type: fmt.Sprintf("mypackage.Bar%dMethodRequest", i),
+						Name: "req",
+					},
+					{
+						Type: "...gax.CallOption",
+						Name: "opts",
+					},
+				},
+			},
+			Segments: []*metadata.Snippet_Segment{
+				{
+					Start: int32(regionTagStart),
+					End:   int32(regionTagEnd - 1),
+					Type:  metadata.Snippet_Segment_FULL,
+				},
+			},
+		}
+		want.Snippets = append(want.Snippets, snp)
+	}
+
 	mi := sm.ToMetadataIndex()
-	// TODO(chrisdsmith): replace assertions with go-cmp(..., proto.EQUAL)
-	cl := mi.ClientLibrary
-	if cl.Name != libPkg {
-		t.Errorf("%s: wanted %s, got %s", t.Name(), libPkg, cl.Name)
-	}
-	if cl.Version != VersionPlaceholder {
-		t.Errorf("%s: wanted %s, got %s", t.Name(), VersionPlaceholder, cl.Version)
-	}
-	if cl.Language != metadata.Language_GO {
-		t.Errorf("%s: wanted %s, got %s", t.Name(), metadata.Language_GO, cl.Language)
-	}
-	if got := len(cl.Apis); got != 1 {
-		t.Errorf("%s: wanted len 1 Apis, got %d", t.Name(), got)
-	}
-	if got := cl.Apis[0].Id; got != protoPkg {
-		t.Errorf("%s: wanted %s, got %s", t.Name(), protoPkg, got)
-	}
-	if got := cl.Apis[0].Version; got != version {
-		t.Errorf("%s: wanted %s, got %s", t.Name(), version, got)
-	}
-	if got := len(mi.Snippets); got != 2 {
-		t.Errorf("%s: wanted len 2 Snippets, got %d", t.Name(), got)
-	}
-	for i, snp := range mi.Snippets {
-		want := fmt.Sprintf("bigquerymigration_v2_generated_Foo%dService_Bar%dMethod_sync", i, i)
-		if got := snp.RegionTag; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("bigquerymigration Bar%dMethod Sample", i)
-		if got := snp.Title; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("Bar%dMethod doc\nNew line.", i)
-		if got := snp.Description; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("Foo%dClient/Bar%dMethod/main.go", i, i)
-		if got := snp.File; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		if snp.Language != metadata.Language_GO {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), metadata.Language_GO, snp.Language)
-		}
-		if snp.Canonical {
-			t.Errorf("%s: wanted Canonical false, got true", t.Name())
-		}
-		cm := snp.ClientMethod
-		want = fmt.Sprintf("Bar%dMethod", i)
-		if got := cm.ShortName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dClient.Bar%dMethod", i, i)
-		if got := cm.FullName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		if cm.Async {
-			t.Errorf("%s: wanted Async false, got true", t.Name())
-		}
-		want = fmt.Sprintf("mypackage.Bar%dMethodResult", i)
-		if got := cm.ResultType; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("Foo%dClient", i)
-		if got := cm.Client.ShortName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dClient", i)
-		if got := cm.Client.FullName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("Bar%dMethod", i)
-		if got := cm.Method.ShortName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dService.Bar%dMethod", i, i)
-		if got := cm.Method.FullName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("Foo%dService", i)
-		if got := cm.Method.Service.ShortName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		want = fmt.Sprintf("google.cloud.bigquery.migration.v2.Foo%dService", i)
-		if got := cm.Method.Service.FullName; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		if got := len(snp.Segments); got != 1 {
-			t.Errorf("%s: wanted len 1 Segments, got %d", t.Name(), got)
-		}
-		if got := snp.Segments[0].Start; got != int32(regionTagStart) {
-			t.Errorf("%s: wanted %d, got %d", t.Name(), regionTagStart, got)
-		}
-		if got := int(snp.Segments[0].End); got != regionTagEnd-1 {
-			t.Errorf("%s: wanted 1, got %d", t.Name(), got)
-		}
-		if got := snp.Segments[0].Type; got != metadata.Snippet_Segment_FULL {
-			t.Errorf("%s: wanted metadata.Snippet_Segment_FULL, got %d", t.Name(), got)
-		}
-		if got := len(cm.Parameters); got != 3 {
-			t.Errorf("%s: wanted len 3 Parameters, got %d", t.Name(), got)
-		}
-		if got := cm.Parameters[0].Type; got != "context.Context" {
-			t.Errorf("%s: wanted context.Context, got %s", t.Name(), got)
-		}
-		if got := cm.Parameters[0].Name; got != "ctx" {
-			t.Errorf("%s: wanted ctx, got %s", t.Name(), got)
-		}
-		want = fmt.Sprintf("mypackage.Bar%dMethodRequest", i)
-		if got := cm.Parameters[1].Type; got != want {
-			t.Errorf("%s: wanted %s, got %s", t.Name(), want, got)
-		}
-		if got := cm.Parameters[1].Name; got != "req" {
-			t.Errorf("%s: wanted req, got %s", t.Name(), got)
-		}
-		if got := cm.Parameters[2].Type; got != "...gax.CallOption" {
-			t.Errorf("%s: wanted ...gax.CallOption, got %s", t.Name(), got)
-		}
-		if got := cm.Parameters[2].Name; got != "opts" {
-			t.Errorf("%s: wanted opts, got %s", t.Name(), got)
-		}
+	if diff := cmp.Diff(mi, want, cmp.Comparer(proto.Equal)); diff != "" {
+		t.Errorf("ToMetadataIndex(): got(-),want(+):\n%s", diff)
 	}
 
 	json, err := sm.ToMetadataJSON()
