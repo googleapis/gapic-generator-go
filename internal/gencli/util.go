4553c7f3b7cd8505a6dfada4ccf5ebf480eb9faa
Julie Qiu <julie@golang.org>
2023-10-05 16:14:03 -0400

    chore(internal/gencli): replace strings.Title
    Fix staticcheck error: SA1019
    
    strings.Title has been deprecated since Go 1.18.
    
    The rule Title uses for word boundaries does not handle Unicode
    punctuation properly.
    
    Use golang.org/x/text/cases instead.

--- internal/gencli/util.go
+++ internal/gencli/util.go
@@ -20,6 +20,8 @@ import (
 
 	"github.com/googleapis/gapic-generator-go/internal/pbinfo"
 	"github.com/jhump/protoreflect/desc"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 const (
@@ -73,7 +75,8 @@ func putImport(imports map[string]*pbinfo.ImportSpec, pkg *pbinfo.ImportSpec) {
 func title(name string) string {
 	split := strings.Split(name, "_")
 	for i, s := range split {
-		split[i] = strings.Title(s)
+		caser := cases.Title(language.English)
+		split[i] = caser.String(s)
 	}
 
 	return strings.Join(split, "")
@@ -82,7 +85,8 @@ func title(name string) string {
 // does not remove underscores
 func dotToCamel(name string) (s string) {
 	for _, tkn := range strings.Split(name, ".") {
-		s += strings.Title(tkn)
+		caser := cases.Title(language.English)
+		s += caser.String(tkn)
 	}
 
 	return
